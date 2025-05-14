4c2efb4a28998056fa4381cd4fe7c68c59ed3f1f
Sowmya Kancharla <vippasowmya0408@gmail.com>
2025-05-14 09:13:54 +0530

    Added git and placed folders

---
+++ 10-sg/sg.tf
@@ -0,0 +1,29 @@
+module "mysql_sg" {
+    source = "../terraform-aws-securitygroup"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "mysql"
+    sg_description = "Created for MySQL instances in expense"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+module "backend_sg" {
+    source = "../terraform-aws-securitygroup"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "backend"
+    sg_description = "Created for MySQL instances in expense"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+module "frontend_sg" {
+    source = "../terraform-aws-securitygroup"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "frontend"
+    sg_description = "Created for MySQL instances in expense"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
