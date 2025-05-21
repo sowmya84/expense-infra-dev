f8602dcc3b3287163bcdd6341eb02a1e0d5b470a
Sowmya Kancharla <vippasowmya0408@gmail.com>
2025-05-21 08:54:36 +0530

    Adding folders

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -42,6 +42,17 @@ module "bastion_sg" {
     common_tags = var.common_tags
 }
 
+module "vpn_sg" {
+    #source = "../terraform-aws-securitygroup"
+    source = "git::https://github.com/sowmya84/terraform-aws-securitygroup.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "vpn"
+    sg_description = "Created for vpn instances in expense"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
 module "app_alb_sg" {
     #source = "../terraform-aws-securitygroup"
     source = "git::https://github.com/sowmya84/terraform-aws-securitygroup.git?ref=main"
@@ -51,4 +62,68 @@ module "app_alb_sg" {
     sg_description = "Created for backend alb in expense"
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = var.common_tags
+}
+
+resource "aws_security_group_rule" "app_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id       = module.bastion_sg.sg_id
+  security_group_id = module.app_alb_sg.sg_id
+}
+
+# JDOPS-32, Bastion host should be accessed from office n/w
+resource "aws_security_group_rule" "bastion_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["103.120.51.33/32"]
+  security_group_id = module.bastion_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_443" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_943" {
+  type              = "ingress"
+  from_port         = 943
+  to_port           = 943
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_1194" {
+  type              = "ingress"
+  from_port         = 1194
+  to_port           = 1194
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "app_alb_vpn" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.vpn_sg.sg_id
+  security_group_id = module.app_alb_sg.sg_id
 }
